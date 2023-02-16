99e73f098e2b8f6da3da4f279d65d63261cf7eb5
Jina <jinayoussef@hotmail.co.uk>
2023-02-16 02:02:49 +0000

    fixed home link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,9 +6,7 @@ const Nav = () => {
   return (
     <div className='nav'>
       
-      <Link to="/">
-        <h1 className='nav__home'>Home</h1>
-      </Link>
+      <Link className='nav__home' to="/">Home</Link>
         <div className='nav__img-container'>
           <img className="nav__logo" src="https://www.now.brewdog.com/sites/template_2018/images/brewdog/shield.svg" alt="logo" />
         </div>
